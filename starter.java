39657eac9ee5a5e71db9caab7cf46613423c9594
pburk34 <p06burk@siena.edu>
2026-02-05 19:58:55 -0500

    starter code

---
+++ starter.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @author Payton Burke
+ * @version 2/5/26
+ * 
+ * code that will generate a random number between 1 and 10, and have the user guess it
+ * 
+ */
+public class starter {
+    public static void main(String args[]) {
+        // Your code goes below here
+        int randomNumber = (int)(Math.random() * 10) + 1;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int userGuess = 0;
+
+        System.out.println("I have selected a random number between 1 and 10. Can you guess it?");
+
+        while (userGuess != randomNumber) {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+
+            if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You've guessed the number " + randomNumber + "!");
+            }
+        }
+
+        scanner.close();
+    }
+}
